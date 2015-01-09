0b84f48e5ab39dd1bce60bbabf970629ff112c3f
Mike Rogne <mike@infinitywebcreations.com>
2015-01-09 02:21:58 -0600

    Added decimal_to_binary.php

---
+++ KhanAcademy/decimal_to_binary.php
@@ -0,0 +1,37 @@
+<?php
+
+var_dump(decimalToBinary(1));
+var_dump(decimalToBinary(2));
+var_dump(decimalToBinary(3));
+
+function decimalToBinary($integer)
+{
+    // Determine maximum power of 2 that will meet or exceed $integer.
+    $highestPower = null;
+    for ($i = 0; $i < 100; $i++) {
+        if (pow(2, $i) >= $integer) {
+            $highestPower = $i;
+            break;
+        }
+    }
+
+    if ($highestPower === null)
+        throw new Exception("Integer too high.");
+
+    $remainder   = $integer;
+    $binaryArray = [];
+    for ($i = $highestPower; $i >= 0; $i--) {
+        // See if pow(2, $i) will go into $remainder.
+        // If it will, push 1. Otherwise push 0.
+        $pow = pow(2, $i);
+
+        if ($remainder / $pow >= 1) {
+            $binaryArray[] = 1;
+            $remainder -= $pow;
+        } else {
+            $binaryArray[] = 0;
+        }
+    }
+
+    return join("", $binaryArray);
+}
