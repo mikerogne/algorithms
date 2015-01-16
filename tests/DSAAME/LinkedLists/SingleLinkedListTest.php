68ded0f553181d2a87bb763a09d311741a2bff6d
Mike Rogne <mike@infinitywebcreations.com>
2015-01-16 00:29:29 -0600

    more tests to cover "count" after modifying list

--- tests/DSAAME/LinkedLists/SingleLinkedListTest.php
+++ tests/DSAAME/LinkedLists/SingleLinkedListTest.php
@@ -26,7 +26,7 @@ class SingleLinkedListTest extends PHPUnit_Framework_TestCase
     {
         $this->list->deleteLastNode();
 
-        $this->assertSame(['first','second'], $this->list->getAllNodes());
+        $this->assertSame(['first', 'second'], $this->list->getAllNodes());
     }
 
     /** @test */
@@ -51,7 +51,7 @@ class SingleLinkedListTest extends PHPUnit_Framework_TestCase
         $this->list->addNodeToBeginning('new starter node!');
 
         $expectedArray = ['new starter node!', 'first', 'second', 'third'];
-        $actualArray = $this->list->getAllNodes();
+        $actualArray   = $this->list->getAllNodes();
 
         $this->assertSame($expectedArray, $actualArray);
     }
@@ -62,7 +62,7 @@ class SingleLinkedListTest extends PHPUnit_Framework_TestCase
         $this->list->addNodeAfter(0, "new node");
 
         $expectedArray = ['first', 'new node', 'second', 'third'];
-        $actualArray = $this->list->getAllNodes();
+        $actualArray   = $this->list->getAllNodes();
 
         $this->assertSame($expectedArray, $actualArray);
     }
@@ -73,7 +73,7 @@ class SingleLinkedListTest extends PHPUnit_Framework_TestCase
         $this->list->addNodeAfter(1, "new node");
 
         $expectedArray = ['first', 'second', 'new node', 'third'];
-        $actualArray = $this->list->getAllNodes();
+        $actualArray   = $this->list->getAllNodes();
 
         $this->assertSame($expectedArray, $actualArray);
     }
@@ -92,7 +92,7 @@ class SingleLinkedListTest extends PHPUnit_Framework_TestCase
     public function can_get_second_node()
     {
         $expected = 'second';
-        $actual = $this->list->getNode(1);
+        $actual   = $this->list->getNode(1);
 
         $this->assertEquals($expected, $actual);
     }
@@ -101,7 +101,95 @@ class SingleLinkedListTest extends PHPUnit_Framework_TestCase
     public function can_get_third_node()
     {
         $expected = 'third';
-        $actual = $this->list->getNode(2);
+        $actual   = $this->list->getNode(2);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test */
+    public function can_delete_second_node()
+    {
+        $this->list->deleteNode(1);
+
+        $expected = ['first', 'third'];
+        $actual   = $this->list->getAllNodes();
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /** @test */
+    public function count_is_correct_after_adding_node_to_beginning()
+    {
+        $this->list->addNodeToBeginning('new node');
+
+        $expected = 4;
+        $actual   = $this->list->getTotalNodes();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test */
+    public function count_is_correct_after_adding_node_to_end()
+    {
+        $this->list->addNode('new node');
+
+        $expected = 4;
+        $actual   = $this->list->getTotalNodes();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test */
+    public function count_is_correct_after_adding_node_in_middle()
+    {
+        $this->list->addNodeAfter(1, 'new node');
+
+        $expected = 4;
+        $actual   = $this->list->getTotalNodes();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test */
+    public function count_is_correct_after_removing_node_from_beginning()
+    {
+        $this->list->deleteFirstNode();
+
+        $expected = 2;
+        $actual   = $this->list->getTotalNodes();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test */
+    public function count_is_correct_after_removing_node_from_end()
+    {
+        $this->list->deleteLastNode();
+
+        $expected = 2;
+        $actual   = $this->list->getTotalNodes();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test */
+    public function count_is_correct_after_removing_node_from_middle()
+    {
+        $this->list->deleteNode(1);
+
+        $expected = 2;
+        $actual   = $this->list->getTotalNodes();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @test */
+    public function count_is_correct_when_deleting_all_nodes()
+    {
+        $this->list->deleteAllNodes();
+
+        $expected = 0;
+        $actual   = $this->list->getTotalNodes();
 
         $this->assertEquals($expected, $actual);
     }
